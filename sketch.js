b07f4b5c361164bb7af98e0ffd3d1ef2e1dfddb7
dsprk414 <73986967+dsprk414@users.noreply.github.com>
2021-02-20 20:09:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var hypnoticBall, database;
+var position;
+var gameState=0
+var playerCount;
+var form;
+var player,game;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+  game=new Game();
+  game.getState()
+  game.start()
+
+
+  
+}
+
+function draw(){
+
+     
+  
+}
+
